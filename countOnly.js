3131c3d2dcb92beb5fa59c19233d9bfc34a72fe8
yusufurur <yusufurur@gmail.com>
2023-02-08 19:05:55 -0500

    Count Only

---
+++ countOnly.js
@@ -0,0 +1,21 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("❌❌❌Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (!results[item]) {
+        results[item] = 1;
+      } else {
+        results[item] += 1;
+      }
+    }
+  }
+  return results;
+};
