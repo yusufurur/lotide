84c074fc8a2afc3820980c5c03a981d69ab71114
yusufurur <yusufurur@gmail.com>
2023-02-09 10:08:22 -0500

    Count Letters

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("❌❌❌Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+onst countLetters = function (str) {
+  const result = {};
+  const newString = str.split(" ").join("");
+  for (const letter of newString) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
