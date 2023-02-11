e208d259281456022d9702ea247d7c51f51e3a9c
yusufurur <yusufurur@gmail.com>
2023-02-11 14:13:35 -0500

    Implement Without

---
+++ without.js
@@ -0,0 +1,14 @@
+const without = function (source, itemsToRemove) {
+  const results = [];
+
+  for (const item of source) {
+    if (!itemsToRemove.includes(item)) {
+      results.push(item);
+    }
+  }
+
+  return results;
+}
+
+let results = without([1, 2, 3, 4], [2, 4]);
+console.log(results);
