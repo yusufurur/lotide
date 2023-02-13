237f98dad604dba2af1bb40fa9d04ba980a788bf
yusufurur <yusufurur@gmail.com>
2023-02-13 15:29:01 -0500

    Implement findKey

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("❌❌❌Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+const findKey = function(obj, callback) {
+  const keys = Object.keys(obj);
+
+  for (const key of keys) {
+    const item = obj[key];
+
+    const result = callback(item);
+
+    if (result) {
+      return key;
+    }
+  }
+};
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(result);
