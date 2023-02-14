bee4f3caabf7ee609da12b22c081d56d18dccfca
yusufurur <yusufurur@gmail.com>
2023-02-14 16:07:40 -0500

    Index

---
+++ index.js
@@ -0,0 +1,35 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without')
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertArraysEqual:  assertArraysEqual,
+  assertEqual:  assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters:  countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+};
