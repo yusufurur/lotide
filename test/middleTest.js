5625add56e41823a0c28a3933ad6107c6d5bddce
yusufurur <yusufurur@gmail.com>
2023-02-14 00:05:17 -0500

    Middle Test

---
+++ test/middleTest.js
@@ -0,0 +1,13 @@
+// test/assertEqualTest.js
+
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+// TEST CODE
+// ...
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // PASS
+// assertArraysEqual(middle([1, 2, 3]), [2]); // PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]); // PASS
+assertArraysEqual(middle([1, 2, 3]), ["2"]); // FAIL
+// assertArraysEqual(middle([1, 2, 3, 4]), [3, 2]); // FAIL
