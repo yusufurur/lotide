fe27a9c8ccdaec9d199c34afa378eaab5b2a0bba
yusufurur <yusufurur@gmail.com>
2023-02-13 23:33:56 -0500

    Head Test

---
+++ test/headTest.js
@@ -0,0 +1,11 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+// TEST CODE
+// ...
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "labs");
