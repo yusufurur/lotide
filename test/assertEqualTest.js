0ed69edb32d6a196bd45f8a88f950e85ab0085a2
yusufurur <yusufurur@gmail.com>
2023-02-13 22:52:29 -0500

    Assert Equal Test

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+// ...
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+module.exports = assertEqual;
