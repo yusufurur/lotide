60221217f2ab9b608945e327d11e0c276080b26d
yusufurur <yusufurur@gmail.com>
2023-02-13 23:54:14 -0500

    Assert Equal Test

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+// test/assertArraysEqual.js
+
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+
+// TEST CODE
+// ...
+
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]);
