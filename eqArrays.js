09b18ff5d52a0f7953d4c452f2ce2cef104591c3
yusufurur <yusufurur@gmail.com>
2023-02-13 23:19:50 -0500

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("❌❌❌Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+  function eqArrays(array1, array2) {
+    if (array1.length !== array2.length) {
+      return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
