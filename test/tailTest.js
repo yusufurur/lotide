9386954984a1bb85dc7305237fb7af63776f1c3e
yusufurur <yusufurur@gmail.com>
2023-02-14 14:48:41 -0500

    Refactor with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,25 @@
 // test/tailTest.js
 
-const tail = require('../tail');
+// const tail = require('../tail');
 
 // TEST CODE
 // ...
 
-console.log(tail([1, 2, 3, 4, 5]));
+// console.log(tail([1, 2, 3, 4, 5]));
 
-module.exports = tail;
+// module.exports = tail;
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.deepEqual(tail([2, 5]), [5]); 
+  });
+
+});
